7e38fc7348679fff62cbb3602ab6915c0b08e25e
cur-iOus <abdullahdotme@gmail.com>
2025-04-27 13:21:00 +0600

    Added index.html to display on home page

--- server.js
+++ server.js
@@ -47,3 +47,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
